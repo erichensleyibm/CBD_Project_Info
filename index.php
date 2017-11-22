a7053fff23b599ade6379ec55e7e55137ca50937
erichensleyibm <eric.hensley@ibm.com>
2017-11-21 20:26:38 -0500

    x

--- index.php
+++ index.php
@@ -80,12 +80,8 @@
 
         ?>
         <tr>
-            <form method = "POST"> <!--FORM: will submit to same page (index.php), and if ($_SERVER["REQUEST_METHOD"] == "POST") will catch it --> 
-                <td colspan = "2">
-                <input type = "text" style = "width:100%" name = "message" autofocus onchange="saveChange(this)" onkeydown="onKey(event)"></input>
-                </td>
+            <form
                 <td>
-                    <button class = "mybutton" type = "submit">Add New Message</button></td></tr>
                 </td> 
             </form>
         </tr>
