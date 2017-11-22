83a696560201db616190c7c6135dea2b7bc477bf
erichensleyibm <eric.hensley@ibm.com>
2017-11-21 20:45:54 -0500

    x

--- index.php
+++ index.php
@@ -1,55 +1,69 @@
-<!--
-/**
-* Copyright 2015 IBM Corp. All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the “License”);
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* https://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an “AS IS” BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-->
+<?php
+/*
+ * Copyright IBM Corp. 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+ /**
+  * This PHP file uses the Slim Framework to construct a REST API.
+  * See Cloudant.php for the database functionality
+  */
+require 'vendor/autoload.php';
+require_once('./Cloudant.php');
+$app = new \Slim\Slim();
+$dotenv = new Dotenv\Dotenv(__DIR__);
+try {
+  $dotenv->load();
+} catch (Exception $e) {
+    error_log("No .env file found");
+ }
+$app->get('/', function () {
+  global $app;
+    $app->render('index.html');
+});
 
-<!DOCTYPE html>
-<html>
+$app->get('/api/visitors', function () {
+  global $app;
+  $app->contentType('application/json');
+  $visitors = array();
+  if(Cloudant::Instance()->isConnected()) {
+    $visitors = Cloudant::Instance()->get();
+  }
+  echo json_encode($visitors);
+});
 
-<head>
-    <title>PHP MySQL Sample Application</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <link rel="stylesheet" href="style.css" />
-</head>
+$app->post('/api/visitors', function() {
+	global $app;
+  $visitor = json_decode($app->request()->getBody(), true);
+  if(Cloudant::Instance()->isConnected()) {
+    Cloudant::Instance()->post($visitor);
+    echo sprintf("Hello %s, I've added you to the database!", $visitor['name']);
+  } else {
+    echo sprintf("Hello %s!", $visitor['name']);
+  }
+});
 
-<body>
-    <div class="">
-        <img class="newappIcon" src="images/newapp-icon.png" />
-        <h1>
-					Welcome to the <span class="blue">PHP MySQL Sample</span> on Bluemix!
-				</h1>
-        <p class="description">This introductory sample allows you to insert messages into a MySQL database. <br>
-            </p>
-            </br>
+$app->delete('/api/visitors/:id', function($id) {
+	global $app;
+	Cloudant::Instance()->delete($id);
+    $app->response()->status(204);
+});
 
-    
-    <table id='notes' class='records'><tbody>
-        
-        <tr>
-            <form
-                <td>
-                </td> 
-            </form>
-        </tr>
-        </tbody>
-    </table>
-    </div>
-</body>
+$app->put('/api/visitors/:id', function($id) {
+	global $app;
+	$visitor = json_decode($app->request()->getBody(), true);
+    echo json_encode(Cloudant::Instance()->put($id, $visitor));
+});
 
-</html>
+$app->run();
